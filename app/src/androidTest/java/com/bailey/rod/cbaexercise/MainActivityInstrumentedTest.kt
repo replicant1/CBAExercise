f35d2c260819b0973b5faed2fc4090340c51044b
rodbailey <rodney.p.bailey@gmail.com>
2021-02-01 18:54:03 +1100

    Add  espresso tests

--- app/src/androidTest/java/com/bailey/rod/cbaexercise/MainActivityInstrumentedTest.kt
+++ app/src/androidTest/java/com/bailey/rod/cbaexercise/MainActivityInstrumentedTest.kt
@@ -1,17 +1,23 @@
 package com.bailey.rod.cbaexercise
 
+import androidx.test.espresso.DataInteraction
+import androidx.test.espresso.Espresso.onData
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions
-import androidx.test.espresso.matcher.ViewMatchers.withId
-import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
 import androidx.test.platform.app.InstrumentationRegistry
+import org.hamcrest.Matchers.equalTo
+import org.hamcrest.Matchers.hasEntry
 import org.junit.Assert.assertEquals
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+
 
 /**
  * Assumes that sample_account_data.json is supplying the data to the UI
@@ -33,9 +39,15 @@ class MainActivityInstrumentedTest {
 
     @Test
     fun accountHeaderIsPopulatedOk() {
-        onView(withId(R.id.tv_account_name)).check(ViewAssertions.matches(withText("Complete Access")))
-        onView(withId(R.id.tv_account_number)).check(ViewAssertions.matches(withText("062005 1709 5888")))
-        onView(withId(R.id.tv_available_funds_value)).check(ViewAssertions.matches(withText("$226.76")))
-        onView(withId(R.id.tv_account_balance_value)).check(ViewAssertions.matches(withText("$246.76")))
+        onView(withId(R.id.tv_account_name)).check(matches(withText("Complete Access")))
+        onView(withId(R.id.tv_account_number)).check(matches(withText("062005 1709 5888")))
+        onView(withId(R.id.tv_available_funds_value)).check(matches(withText("$226.76")))
+        onView(withId(R.id.tv_account_balance_value)).check(matches(withText("$246.76")))
     }
+
+    @Test
+    fun secondRowIsCorrectDateHeader() {
+        onView(withText("20 JUL 2017")).check(matches(isDisplayed()))
+    }
+
 }
