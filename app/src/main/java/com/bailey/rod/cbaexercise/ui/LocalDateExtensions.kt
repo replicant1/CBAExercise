a21e9c1fd8f02a53aa590525d04d5554234007ff
rodbailey <rodney.p.bailey@gmail.com>
2021-02-13 22:10:06 +1100

    Removed DaysAgoTest.kt. Modifying DaysAgoLabelTest.kt

--- app/src/main/java/com/bailey/rod/cbaexercise/ui/LocalDateExtensions.kt
+++ app/src/main/java/com/bailey/rod/cbaexercise/ui/LocalDateExtensions.kt
@@ -1,24 +1,32 @@
 package com.bailey.rod.cbaexercise.ui
 
 import java.time.LocalDate
-import java.time.temporal.ChronoField
 
-fun LocalDate.daysAgo(oldDate: LocalDate): Long {
-    val thisEpochDay = this.getLong(ChronoField.EPOCH_DAY)
-    val oldEpochDay = oldDate.getLong(ChronoField.EPOCH_DAY)
-    return thisEpochDay - oldEpochDay
-}
 
-fun LocalDate.daysAgoLabel(oldDate: LocalDate): String {
-    val daysAgo = this.daysAgo(oldDate)
-    if (daysAgo == 0L) {
-        return "Today"
-    } else if (daysAgo == 1L) {
-        return "$daysAgo day ago"
-    } else if (daysAgo > 1L) {
-        return "$daysAgo days ago"
-    }
-    else {
-        return "?"
+/**
+ * @param Must be in the past
+ * @return String representation of the age of something born on the given date.
+ *
+ * For ages in [0, 1 month) show "X days ago". Special cases if x == 0 then "today". if x == 1 then "yesterday".
+ * For ages [1 month, 12 months) show "X months ago". special cases if x == 1 then "last month".
+ * For ages [1 year, -) show "X years ago". Special case: if x == 1 then "last year".
+ */
+fun LocalDate.daysAgoLabel(then: LocalDate): String {
+    val yearDiff = this.year - then.year
+
+    if (yearDiff > 0) {
+        return if (yearDiff == 1) "Last year" else "$yearDiff years ago"
+    } else {
+        val monthDiff = this.monthValue - then.monthValue
+        if (monthDiff > 0) {
+            return if (monthDiff == 1) "Last month" else "$monthDiff months ago"
+        } else {
+            val dayDiff = this.dayOfMonth - then.dayOfMonth
+            if (dayDiff == 0) {
+                return "Today"
+            } else {
+                return if (dayDiff == 1) "Yesterday" else "$dayDiff days ago"
+            }
+        }
     }
 }
--- app/src/test/java/com/bailey/rod/cbaexercise/DaysAgoLabelTest.kt
+++ app/src/test/java/com/bailey/rod/cbaexercise/DaysAgoLabelTest.kt
@@ -9,29 +9,36 @@ class DaysAgoLabelTest {
 
     @Test
     fun todayIsLabelledToday() {
-        val today = LocalDate.now()
+        val today = LocalDate.of(2021, 2, 13) // 13 Feb 2021
         assertEquals("Today", today.daysAgoLabel(today))
     }
 
     @Test
-    fun yesterdayIsLabelledOneDayAgo() {
-        val today = LocalDate.now()
+    fun yesterdayIsLabelledYesterday() {
+        val today = LocalDate.of(2021, 2, 13) // 13 Feb 2021
         val yesterday = today.minusDays(1)
-        assertEquals("1 day ago", today.daysAgoLabel(yesterday))
+        assertEquals("Yesterday", today.daysAgoLabel(yesterday))
     }
 
     @Test
-    fun dayBeforeYesterdayIsLabelledTwoDaysAgo() {
-        val today = LocalDate.now()
-        val yesterday = today.minusDays(1)
-        val dayBeforeYesterday = yesterday.minusDays(1)
-        assertEquals("2 days ago", today.daysAgoLabel(dayBeforeYesterday))
+    fun twoDaysAgoIsLabelled2DaysAgo() {
+        val today = LocalDate.of(2021, 2, 13)
+        val twoDaysAgo = LocalDate.now().minusDays(2)
+        assertEquals("2 days ago", today.daysAgoLabel(twoDaysAgo))
     }
 
     @Test
-    fun tomorrowIsLabelledUnknown() {
-        val today = LocalDate.now()
-        val tomorrow = today.plusDays(1)
-        assertEquals("?", today.daysAgoLabel(tomorrow))
+    fun oneMonthAgoIsLabelledLastMonth() {
+        val today = LocalDate.of(2021, 2, 13)
+        val oneMonthAgo = today.minusMonths(1)
+        assertEquals("Last month", today.daysAgoLabel(oneMonthAgo))
     }
+
+    @Test
+    fun eleventMonthsAgoIsLabelled11MonthsAgo() {
+        val today = LocalDate.of(2020, 12, 25) // 25 Dec 2020
+        val elevenMonthsAgo = today.minusMonths(11)
+        assertEquals("11 months ago", today.daysAgoLabel(elevenMonthsAgo))
+    }
+
 }
